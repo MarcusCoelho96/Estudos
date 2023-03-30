66a1d3583c983867ed0ac680efe01b705ff9249e
MarcusCoelho96 <97898642+MarcusCoelho96@users.noreply.github.com>
2023-03-30 13:36:12 -0300

    Formulário
    Criação de formulário de cadastro de usuário com validações feitas em javascript.

---
+++ html-css/exercicios/outros/ex001/js/script.js
@@ -0,0 +1,109 @@
+
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordConfirmation = document.getElementById("password-confirmation");
+
+/*---------------------------------------------------- */
+form.addEventListener("submit", (event) =>{
+    event.preventDefault();
+
+    checkForm();
+});
+
+username.addEventListener("blur", () =>{
+    checkInputUsername();
+});
+
+email.addEventListener("blur", () =>{
+    checkInputEmail();
+});
+
+password.addEventListener("blur", () =>{
+    checkInputPassword();
+});
+
+passwordConfirmation.addEventListener("blur", () =>{
+    checkInputPasswordConfirmation();
+});
+/*---------------------------------------------------- */
+
+function checkForm(){
+    checkInputUsername();
+    checkInputEmail();
+    checkInputPassword();
+    checkInputPasswordConfirmation();
+
+    const formItems = form.querySelectorAll(".form-content");
+
+    const isValid = [...formItems].every((item) =>{
+        return item.className === "form-content";
+    });
+
+    if(isValid){
+        alert("Cadastrado com sucesso!");
+    }
+}
+/*---------------------------------------------------- */
+
+function errorInput(input, message){
+    const formItem = input.parentElement;
+    const textMessage = formItem.querySelector("a");
+
+    textMessage.innerText = message;
+
+    formItem.className = "form-content error";
+}
+
+function okInput(input){
+    const formItem = input.parentElement;
+    formItem.className = "form-content";
+}
+/*---------------------------------------------------- */
+
+function checkInputUsername(){
+    const usernameValue = username.value;
+        
+    if(usernameValue === ""){
+        errorInput(username, "Preencha com o seu nome.");
+    }else{
+        okInput(username);
+    }
+
+}
+
+function checkInputEmail(){
+    const emailValue = email.value;
+
+    if(emailValue === ""){
+        errorInput(email, "Preencha com o seu email.");
+    }else{
+        okInput(email);
+    }
+}
+
+function checkInputPassword(){
+    const passwordValue = password.value;
+
+    if(passwordValue === ""){
+        errorInput(password, "Insira uma senha de 8 dígitos no mínimo.");
+    }else if(passwordValue.length < 8){
+        errorInput(password, "Sua senha deve conter no mínimo 8 dígitos.");
+    }else{
+        okInput(password);
+    }
+}
+
+function checkInputPasswordConfirmation(){
+    const passwordConfirmationValue = passwordConfirmation.value;
+    const passwordValue = password.value;
+
+    if(passwordConfirmationValue === ""){
+        errorInput(passwordConfirmation, "Insira sua senha novamente.");
+    }else if(passwordConfirmationValue != passwordValue){
+        errorInput(passwordConfirmation, "As senhas devem ser iguais.");
+    }else{
+        okInput(passwordConfirmation);
+    }
+}
