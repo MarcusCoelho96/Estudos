3dcbe66831e9b3043b23e140880b071fd51e350d
MarcusCoelho96 <97898642+MarcusCoelho96@users.noreply.github.com>
2023-04-03 13:22:06 -0300

    interação com tamanho da tela

--- html-css/exercicios/modulo04/ex004/js/script.js
+++ html-css/exercicios/modulo04/ex004/js/script.js
@@ -5,3 +5,12 @@ function mostrarItens(){
         itens.style.display = 'none';
     }
 }
+function tamanhoTela(){
+    if(window.innerWidth >= 789){
+        menu.style.display = 'none';
+        itens.style.display = 'block';
+    }else{
+        menu.style.display = 'block';
+        itens.style.display = 'none';
+    }
+}
