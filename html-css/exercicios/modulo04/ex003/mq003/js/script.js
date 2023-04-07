7588451478b8ff5f6f7081083d74a7a44df3225b
MarcusCoelho96 <97898642+MarcusCoelho96@users.noreply.github.com>
2023-04-07 14:30:39 -0300

    mudanças para onresize="function()"

--- html-css/exercicios/modulo04/ex003/mq003/js/script.js
+++ html-css/exercicios/modulo04/ex003/mq003/js/script.js
@@ -5,3 +5,10 @@ function mostrarItens(){
         itens.style.display = 'none';
     }
 }
+function tirarBurguer(){
+    if(window.screen.width >= 612){
+        itens.style.display = 'block';
+    }else if(window.screen.width < 612){
+        itens.style.display = 'none';
+    }
+}
