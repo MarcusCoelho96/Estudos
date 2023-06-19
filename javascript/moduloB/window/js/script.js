0eb71ebbe6ac26a6745de164f65bcf036bf8dd1b
MarcusCoelho96 <97898642+MarcusCoelho96@users.noreply.github.com>
2023-06-19 17:40:07 -0300

    Window
    pergunta, alerta, confirm

---
+++ javascript/moduloB/window/js/script.js
@@ -0,0 +1,6 @@
+/* alt - shift - a, para comentar o código.
+window.alert('Minha primeira mensagem!');
+window.confirm('Está gostando de JS?'); 
+*/
+var nome = window.prompt("Digite o seu nome: ");
+window.alert("Esse é o seu nome: " + nome);
