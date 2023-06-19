5f4a1d4ccedd372fcf1229594ccaadbd698c1225
MarcusCoelho96 <97898642+MarcusCoelho96@users.noreply.github.com>
2023-06-19 17:39:30 -0300

    Number
    converter string para number

---
+++ javascript/moduloB/number/js/script.js
@@ -0,0 +1,8 @@
+/* alt - shift - a, para comentar o código.
+window.alert('Minha primeira mensagem!');
+window.confirm('Está gostando de JS?'); 
+*/
+var number1 = Number(window.prompt("Digite um número"));
+var number2 = Number(window.prompt("Digite outro número"));
+var soma = number1 + number2;
+window.alert(number1 + " + " + number2 + " = " + soma);
