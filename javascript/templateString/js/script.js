ef030c7075eacf890aa5678064f15ef50bd9908a
MarcusCoelho96 <97898642+MarcusCoelho96@users.noreply.github.com>
2023-06-19 17:40:38 -0300

    Template String
    tipo de concatenação

---
+++ javascript/templateString/js/script.js
@@ -0,0 +1,13 @@
+/* alt - shift - a, para comentar o código.
+window.alert('Minha primeira mensagem!');
+window.confirm('Está gostando de JS?'); 
+*/
+alert("teste");
+
+var nome = String(window.prompt("Digite o nome do aluno: "));
+
+var idade = Number.parseInt(window.prompt("Digite a idade do aluno"));
+var nota = Number.parseFloat(window.prompt("Digite a nota do aluno"));
+window.alert(`O aluno ${nome} com ${idade} anos tirou a nota ${nota}`); /* Usar `crase` */
+
+
